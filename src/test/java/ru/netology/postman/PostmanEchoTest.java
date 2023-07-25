eeafb68a84c1dd1065bf9d43d46e9b669c5d98f9
Evgesha <Slav.novikova2014@gmail.com>
2023-07-25 14:05:47 +0700

    wrong test

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -13,6 +13,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hi"));
+                .body("data", equalTo("He"));
     }
 }
